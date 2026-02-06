45bf93a114ea4971f46717c32ffba4298044b37c
Aryelson2006 <aryelsonfreitasdosantos@gmail.com>
2026-02-05 21:39:05 -0300

    atualização arquivo

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Minha primeira página</title>
+    <link rel="stylesheet" href="assets/styles.css">
 </head>
+
 <body>
-    
+    <div class="container">
+
+        <!-- CABEÇALHO DA MINHA PÁGINA  -->
+        <header>
+            <!-- IMAGEM -->
+            <img src="assets/ifto-logo.png" alt="ifto-logo">
+
+            <!-- TÍTULO -->
+            <h1>Esse é meu primeiro projeto WEB</h1>
+            <p>Me chamo Aryelson aluno do 3° período</p>
+        </header>
+
+        <!-- MENU DE NAVEGAÇÃO -->
+        <nav>
+            <ul>
+                <li><a href="#">Tags HTML</a></li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+
+        </nav>
+
+
+
+    </div>
+
 </body>
+
 </html>
